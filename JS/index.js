726c1683539de58cd9fa74b59bec9d891879ffe4
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-03-14 12:42:32 +0300

    Feature;Adding styles and scripting my project

--- JS/index.js
+++ JS/index.js
@@ -9,14 +9,30 @@ $(document).ready(function() {
     $("form#new-contact").submit(function(event) {
         event.preventDefault();
 
+
+        Contact.prototype.fullName = function() {
+            return this.firstName + " " + this.lastName;
+        }
+
+
         var inputtedFirstName = $("input#new-first-name").val();
         var inputtedLastName = $("input#new-last-name").val();
 
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + " </span></li> ");
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+        // $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + " </span></li> ");
 
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
     });
+
+
+    $(".contact").last().click(function() {
+        $("#show-contact").show();
+        $("#show-contact h2").text(newContact.firstName);
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+    });
+
 });
